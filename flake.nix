07cc02ac9a9ea3f6f78a6c9f05678989df356863
Radosław Grochowski <rg@fard.pl>
2024-09-25 19:37:06 +0200

    feat: initial configuration

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Stratus Homeserver configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    nixosConfigurations.stratus = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  } // flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            just
+          ];
+        };
+      });
+}
