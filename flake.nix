db9aad7c00a5156226ee309b8c18595261a42706
Radosław Grochowski <rg@fard.pl>
2025-04-18 12:31:54 +0200

    feat: setup CI

--- flake.nix
+++ flake.nix
@@ -34,4 +34,16 @@
           ];
         };
       });
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://radoslawgrochowski-homeserver.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "radoslawgrochowski-homeserver.cachix.org-1:IUdvjxx3+DE0HQC2sxfrh0WWi9UlZBUGgi0CJz4K3DI="
+    ];
+  };
 }
