f35fdf80ced6511b92fef465a36868994ba75d20
Radosław Grochowski <rg@fard.pl>
2025-07-16 20:00:44 +0200

    feat(jellyfin): add skip intro button placeholder

--- configuration/jellyfin.nix
+++ configuration/jellyfin.nix
@@ -1,4 +1,21 @@
 { pkgs, ... }: {
+  nixpkgs.overlays = [
+    (
+      final: prev:
+        {
+          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+            installPhase = ''
+              runHook preInstall
+              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+              mkdir -p $out/share
+              cp -a dist $out/share/jellyfin-web
+              runHook postInstall
+            '';
+          });
+        }
+    )
+  ];
+
   services.jellyfin = {
     enable = true;
     user = "media";
