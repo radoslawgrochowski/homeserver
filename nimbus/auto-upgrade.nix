4b4332af56cc4d1b325aa8a7270c57df1bfd2165
Radosław Grochowski <rg@fard.pl>
2025-08-27 16:38:10 +0200

    feat(nimbus): enable auto-upgrade

--- nimbus/auto-upgrade.nix
+++ nimbus/auto-upgrade.nix
@@ -1,13 +1,8 @@
 { ... }:
 {
   system.autoUpgrade = {
-    enable = false;
-    allowReboot = true;
+    enable = true;
     flake = "github:radoslawgrochowski/homeserver";
-    dates = "04:10";
-    rebootWindow = {
-      lower = "04:00";
-      upper = "05:00";
-    };
+    dates = "04:40";
   };
 }
